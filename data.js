98ebc4d99ae885c4aa3f084bb351fcb08aa0ee1a
Andrea-gif23 <andreagutierrezromero98@gmail.com>
2025-01-02 20:14:53 +0100

    ejercicio terminado

---
+++ data.js
@@ -0,0 +1,10 @@
+
+const pageData = {
+    title: 'Mi Página Web',
+    subtitle: 'Bienvenidos a mi servidor HTTP',
+    description: 'Esta es una página generada dinámicamente con Node.js.',
+  };
+  
+ 
+  module.exports = pageData;
+  
